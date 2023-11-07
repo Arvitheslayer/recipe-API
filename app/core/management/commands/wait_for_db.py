e238e09a967cac45b9acfc1905249ec41cbc173d
Andrii <89351600+Arvitheslayer@users.noreply.github.com>
2023-11-07 07:26:29 +0100

    Add new features and tests for them

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,7 +7,6 @@ from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
-
 class Command(BaseCommand):
     # Django command to wait for database.
     def handle(self, *args, **options):
