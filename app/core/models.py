281822b2410fd4b2b0c89889eff7ed1922528511
Andrii <89351600+Arvitheslayer@users.noreply.github.com>
2023-11-07 14:55:35 +0100

    Tests for user models and features added

--- app/core/models.py
+++ app/core/models.py
@@ -20,6 +20,14 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        # Create and return new super user.
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     # User in the system
