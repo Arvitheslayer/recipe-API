d177ed630f29fd8d97419c4de0fce04f0f14fc67
Andrii <89351600+Arvitheslayer@users.noreply.github.com>
2023-11-14 17:13:23 +0100

    Added test and feature of modifying users

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,6 +2,7 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from . import models
 
@@ -10,6 +11,21 @@ class UserAdmin(BaseUserAdmin):
     # Define the admin for users.
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
