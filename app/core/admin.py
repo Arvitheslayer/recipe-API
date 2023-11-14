a288d3710d06d63a7a385df33ae2366d56ee2f83
Andrii <89351600+Arvitheslayer@users.noreply.github.com>
2023-11-14 07:28:14 +0100

    Added test and feature listing admin users

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,14 @@
-# from django.contrib import admin
+# Django admin customization.
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from . import models
+
+class UserAdmin(BaseUserAdmin):
+    # Define the admin for users.
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
