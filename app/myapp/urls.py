0eb038d54f44897a47c7415f1f00550771804c0e
Andrii <89351600+Arvitheslayer@users.noreply.github.com>
2023-11-23 19:31:31 +0100

    Implemented recipe listing API and test for it.

--- app/myapp/urls.py
+++ app/myapp/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for app project.
+URL configuration for myapp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -30,4 +30,5 @@ urlpatterns = [
         name='api-docs',
     ),
     path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls'))
 ]
---
+++ app/recipe/urls.py
@@ -0,0 +1,13 @@
+# URL mappings for the recipe myapp.
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from app.recipe import views
+
+router = DefaultRouter
+router.register('recipes', views.RecipeViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
